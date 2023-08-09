9bc8854bf057e3533821eb4bc13884a190278aac
HEENA JAIN <jainheenacse@gmail.com>
2023-08-09 11:42:47 +0530

    commit

--- Arrays/sort0s1s2s.py
+++ Arrays/sort0s1s2s.py
@@ -6,15 +6,16 @@ class Solution:
         low=0
         mid=0
         high=n-1
+        #dutch flag algorithm
         while mid<=high:
             if arr[mid]==0:
-                arr[mid],arr[low]=arr[low],arr[mid]
+                arr[mid],arr[low]=arr[low],arr[mid] # if mid value is 0 swap it with low value
                 mid+=1
                 low+=1
             elif arr[mid]==1:
-                mid+=1
+                mid+=1 # if mid value is 1 then no change and increase mid by 1
             else:
-                arr[mid],arr[high]=arr[high],arr[mid]
+                arr[mid],arr[high]=arr[high],arr[mid] # if mid is 2 swap it with high value and decrease high by 1
                 high-=1
 
 
