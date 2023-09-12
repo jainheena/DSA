384b7bbb1c14ad6127fc329ae6aff36f2936bdca
HEENA JAIN <jainheenacse@gmail.com>
2023-09-12 13:37:46 +0530

    commit

--- backtracking/quickSort.py
+++ backtracking/quickSort.py
@@ -1,36 +1,42 @@
+#User function Template for python3
+
 class Solution:
-    @staticmethod
-    def swap(a, b, arr):
-        temp = arr[a]
-        arr[a] = arr[b]
-        arr[b] = temp
-
-    def quickSort(self, arr, low, high):
-        if low < high:
-            part = self.partition(arr, low, high)
-            self.quickSort(arr, low, part - 1)
-            self.quickSort(arr, part + 1, high)
-
-    def partition(self, arr, low, high):
-        pivot = arr[low]
-        i=low+1
-        j = high
-
-        for j in range(high,pivot+1,-1):
+    #Function to sort a list using quick sort algorithm.
+    def swap(self,nums,i,j):
+        temp=nums[i]
+        nums[i]=nums[j]
+        nums[j]=temp
+        
+    def quickSort(self,arr,low,high):
+        # code here
+        if low<high:
+            part=self.partition(arr,low,high)
+            self.quickSort(arr,low,part-1)
+            self.quickSort(arr,part+1,high)
+    
+    def partition(self,arr,low,high):
+        # code here
+        pivot=arr[high]
+        i=low-1
+        for j in range(low,high):
             if arr[j]<pivot:
                 i+=1
-                Solution.swap(i, j, arr)
-
-        Solution.swap(low, j, arr)
-        return j
-
+                self.swap(arr,i,j)
+        self.swap(arr,i+1,high)
+        return i+1
+    
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
 
 if __name__ == "__main__":
-    t = int(input())
+    t=int(input())
     for i in range(t):
-        n = int(input())
-        arr = list(map(int, input().split()))
-        Solution().quickSort(arr, 0, n - 1)
+        n=int(input())
+        arr=list(map(int,input().split()))
+        Solution().quickSort(arr,0,n-1)
         for i in range(n):
-            print(arr[i], end=" ")
+            print(arr[i],end=" ")
         print()
+
+# } Driver Code Ends
